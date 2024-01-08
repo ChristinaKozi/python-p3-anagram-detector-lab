7b462d3a4cfb9afaea0ed459ce563370fba2f0e8
Christina Kozanian <christinakozanian@gmail.com>
2024-01-08 15:42:53 -0800

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        matching = []
+        for word in list:
+            if sorted(self.word) == sorted(word):
+                matching.append(word)
+        return matching
